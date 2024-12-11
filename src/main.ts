7a0f333b91345d59185adef86bfd24e4191de524
Anna <64531297+AnnFyz@users.noreply.github.com>
2024-12-11 04:05:00 +0100

    add models with animations

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import Stats from "three/addons/libs/stats.module.js";
 
 const scene = new THREE.Scene();
+
+const gridHelper = new THREE.GridHelper(100, 100);
+scene.add(gridHelper);
 
 new RGBELoader().load("img/venice_sunset_1k.hdr", (texture) => {
   texture.mapping = THREE.EquirectangularReflectionMapping;
   scene.environment = texture;
   scene.background = texture;
-  scene.backgroundBlurriness = 0.5;
+  scene.backgroundBlurriness = 1;
 });
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.position.set(0, 0, 3);
+camera.position.set(0.1, 1, 1);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 0.8;
-renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -32,85 +32,55 @@ window.addEventListener("resize", () => {
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
-
-const raycaster = new THREE.Raycaster();
-const pickables: THREE.Mesh[] = [];
-const mouse = new THREE.Vector2();
-
-const arrowHelper = new THREE.ArrowHelper();
-arrowHelper.setLength(0.5);
-scene.add(arrowHelper);
-
-renderer.domElement.addEventListener("mousemove", (e) => {
-  mouse.set((e.clientX / renderer.domElement.clientWidth) * 2 - 1, -(e.clientY / renderer.domElement.clientHeight) * 2 + 1);
-
-  raycaster.setFromCamera(mouse, camera);
-
-  const intersects = raycaster.intersectObjects(pickables, false);
-
-  if (intersects.length) {
-    //console.log(intersects.length)
-    //console.log(intersects[0].point)
-    //console.log(intersects[0].object.name + ' ' + intersects[0].distance)
-    //console.log((intersects[0].face as THREE.Face).normal)
-
-    const n = new THREE.Vector3();
-    n.copy((intersects[0].face as THREE.Face).normal);
-    //n.transformDirection(intersects[0].object.matrixWorld)
-
-    arrowHelper.setDirection(n);
-    arrowHelper.position.copy(intersects[0].point);
-  }
-});
-
-renderer.domElement.addEventListener("dblclick", (e) => {
-  mouse.set((e.clientX / renderer.domElement.clientWidth) * 2 - 1, -(e.clientY / renderer.domElement.clientHeight) * 2 + 1);
-
-  raycaster.setFromCamera(mouse, camera);
-
-  const intersects = raycaster.intersectObjects(pickables, false);
-
-  if (intersects.length) {
-    const n = new THREE.Vector3();
-    n.copy((intersects[0].face as THREE.Face).normal);
-    //n.transformDirection(intersects[0].object.matrixWorld)
-
-    const cube = new THREE.Mesh(new THREE.BoxGeometry(0.2, 0.2, 0.2), new THREE.MeshStandardMaterial());
-    cube.lookAt(n);
-    cube.position.copy(intersects[0].point);
-    cube.position.addScaledVector(n, 0.1);
-    cube.castShadow = true;
-
-    scene.add(cube);
-    pickables.push(cube);
-  }
-});
-
-new GLTFLoader().load("models/suzanne_scene.glb", (gltf) => {
-  const suzanne = gltf.scene.getObjectByName("Suzanne") as THREE.Mesh;
-  suzanne.castShadow = true;
-  // @ts-ignore
-  suzanne.material.map.colorSpace = THREE.LinearSRGBColorSpace;
-  pickables.push(suzanne);
-
-  const plane = gltf.scene.getObjectByName("Plane") as THREE.Mesh;
-  plane.receiveShadow = true;
-  pickables.push(plane);
-
-  const spotLight = gltf.scene.getObjectByName("Spot") as THREE.SpotLight;
-  spotLight.intensity /= 500;
-  spotLight.castShadow = true;
-
-  scene.add(gltf.scene);
-});
+controls.target.set(0, 0.75, 0);
 
 const stats = new Stats();
 document.body.appendChild(stats.dom);
 
+let mixer: THREE.AnimationMixer;
+let animationActions: { [key: string]: THREE.AnimationAction } = {};
+let activeAction: THREE.AnimationAction;
+
+async function loadEve() {
+  const loader = new GLTFLoader();
+  // const [eve, idle, run] = await Promise.all([
+  //   loader.loadAsync("models/pop_skin_walk_1.glb"),
+  //   loader.loadAsync("models/eve@idle.glb"),
+  //   loader.loadAsync("models/eve@run.glb"),
+  // ]);
+  const [idle] = await Promise.all([
+    loader.loadAsync("models/pop_skin_walk_1.glb"),
+    loader.loadAsync("models/eve@idle.glb"),
+    loader.loadAsync("models/eve@run.glb"),
+  ]);
+
+  mixer = new THREE.AnimationMixer(idle.scene);
+
+  mixer.clipAction(idle.animations[2]).play();
+
+  animationActions["idle"] = mixer.clipAction(idle.animations[2]);
+  // animationActions["walk"] = mixer.clipAction(eve.animations[0]);
+  // animationActions["run"] = mixer.clipAction(run.animations[0]);
+
+  animationActions["idle"].play();
+  activeAction = animationActions["idle"];
+
+  scene.add(idle.scene);
+  idle.scene.scale.multiplyScalar(0.5);
+}
+await loadEve();
+
+const clock = new THREE.Clock();
+let delta = 0;
+
 function animate() {
   requestAnimationFrame(animate);
 
+  delta = clock.getDelta();
+
   controls.update();
+
+  mixer.update(delta);
 
   renderer.render(scene, camera);
 

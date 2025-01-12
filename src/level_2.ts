173852612ff5ebbd2e549a10f6d7b9bb854f9846
Anna <64531297+AnnFyz@users.noreply.github.com>
2025-01-12 20:15:56 +0100

    add

--- src/level_2.ts
+++ src/level_2.ts
@@ -1,9 +1,195 @@
 import * as THREE from "three";
+import "./style.css";
+//import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { GUI } from "dat.gui";
+import Button from "./button";
+import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
+import { FontLoader } from "three/addons/loaders/FontLoader.js";
+import { OutlinePass } from "three/addons/postprocessing/OutlinePass.js";
 
 export default class Level_2 {
   scene = new THREE.Scene();
-
-  constructor() {
+  camera: THREE.PerspectiveCamera;
+  renderer: THREE.Renderer;
+  raycaster: THREE.Raycaster;
+  buttons: Button[] = [];
+  mouse: THREE.Vector2;
+  ambientLight: THREE.AmbientLight;
+  pointLight: THREE.PointLight;
+  outlinePass: any;
+  textMesh = new THREE.Mesh();
+  cube = new THREE.Mesh();
+  constructor(camera: THREE.PerspectiveCamera, renderer: THREE.Renderer, raycaster: THREE.Raycaster, mouse: THREE.Vector2) {
+    this.camera = camera;
+    this.renderer = renderer;
+    this.raycaster = raycaster;
+    this.mouse = mouse;
+    //light
+    this.ambientLight = new THREE.AmbientLight(0xffffff, 2);
+    this.pointLight = new THREE.PointLight(0xffffff, 500);
     this.scene.background = new THREE.Color(0x654321);
+    this.init();
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x123456 });
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+  }
+
+  init() {
+    //this.createGridCameraUI();
+    this.createBackground();
+    this.createLight();
+    this.createButton();
+    this.createTextMesh();
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshNormalMaterial({ wireframe: true });
+
+    const cube = new THREE.Mesh(geometry, material);
+    this.scene.add(cube);
+  }
+
+  createGridCameraUI() {
+    this.camera.position.set(0.1, 2, 6);
+
+    const gridHelper = new THREE.GridHelper(100, 100);
+    let isGridVisible = {
+      switch: true,
+    };
+    gridHelper.visible = isGridVisible.switch;
+    this.scene.add(gridHelper);
+
+    // UI
+    const gui = new GUI();
+    const cameraFolder = gui.addFolder("Camera");
+    cameraFolder
+      .add(isGridVisible, "switch")
+      .name("grid visibility")
+      .onChange(() => {
+        gridHelper.visible = isGridVisible.switch;
+      });
+    cameraFolder.add(this.camera.position, "x", -10, 10);
+    cameraFolder.add(this.camera.position, "y", -10, 10);
+    cameraFolder.add(this.camera.position, "z", -10, 10);
+    cameraFolder.add(this.camera, "fov", 0, 180, 0.01).onChange(() => {
+      this.camera.updateProjectionMatrix();
+    });
+    cameraFolder.add(this.camera, "aspect", 0.00001, 10).onChange(() => {
+      this.camera.updateProjectionMatrix();
+    });
+    cameraFolder.add(this.camera, "near", 0.01, 10).onChange(() => {
+      this.camera.updateProjectionMatrix();
+    });
+    cameraFolder.add(this.camera, "far", 0.01, 10).onChange(() => {
+      this.camera.updateProjectionMatrix();
+    });
+
+    cameraFolder.open();
+  }
+
+  createBackground() {
+    const environmentTexture = new THREE.TextureLoader().load("img/Background_1.png");
+    this.scene.environment = environmentTexture;
+    this.scene.background = environmentTexture;
+  }
+
+  createLight() {
+    const light = new THREE.PointLight(0xffffff, 500);
+    light.position.set(10, 10, 10);
+    this.scene.add(light);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.25);
+    this.scene.add(ambientLight);
+  }
+
+  setOutlines(outlinePass: OutlinePass) {
+    this.outlinePass = outlinePass;
+  }
+
+  createTextMesh() {
+    let textScale = 0.1;
+    let textOffset = 3;
+    let textGeo: TextGeometry;
+    let textMesh = this.textMesh;
+    let activeScene = this.scene;
+    let outlinePass = this.outlinePass;
+    const loader = new FontLoader();
+    loader.load("fonts/Play_Regular.json", function (font) {
+      textGeo = new TextGeometry("Welcome to the magic world \n of the Adventure Pup !", {
+        font: font,
+        size: 1,
+        height: 0.25,
+        curveSegments: 8,
+        bevelEnabled: true,
+        bevelThickness: 0.125,
+        bevelSize: 0.025,
+        bevelOffset: 0,
+        bevelSegments: 4,
+      });
+      textGeo.computeBoundingBox();
+      textMesh = new THREE.Mesh(textGeo, new THREE.MeshToonMaterial({ color: 0x3f54ff }));
+      if (textGeo.boundingBox != null) {
+        const centerOffset = -0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x) * textScale;
+        textMesh.position.x = centerOffset * textOffset;
+        textMesh.position.y = 2;
+        textMesh.position.z = 0;
+
+        textMesh.rotation.x = 0;
+        textMesh.rotation.y = Math.PI * 0.1;
+        textMesh.scale.set(textScale, textScale, textScale);
+      }
+      activeScene.add(textMesh);
+      //outlinePass.selectedObjects.push(textMesh);
+    });
+  }
+
+  createButton() {
+    new GLTFLoader().load("models/Button_1.glb", (gltf) => {
+      const buttonMesh = gltf.scene.getObjectByName("Button") as THREE.Mesh;
+      const button = new Button(buttonMesh.geometry, new THREE.MeshToonMaterial({ color: 0x3f54ff }), new THREE.Color(0x0088ff));
+      button.setScale(0.3, 0.3, 0.3);
+      button.setPosition(-1, 1, -0.15);
+      console.log(button);
+      // @ts-ignore
+      this.buttons.push(button);
+      this.scene.add(button);
+    });
+
+    this.renderer.domElement.addEventListener("mousemove", (e) => {
+      const intersects = this.raycaster.intersectObjects(this.buttons, false);
+      this.mouse.set(
+        (e.clientX / this.renderer.domElement.clientWidth) * 2 - 1,
+        -(e.clientY / this.renderer.domElement.clientHeight) * 2 + 1
+      );
+      this.raycaster.setFromCamera(this.mouse, this.camera);
+
+      this.buttons.forEach((p) => (p.hovered = false));
+      if (intersects.length) {
+        (intersects[0].object as Button).hovered = true;
+      }
+    });
+
+    this.renderer.domElement.addEventListener("pointerdown", (e) => {
+      this.mouse.set(
+        (e.clientX / this.renderer.domElement.clientWidth) * 2 - 1,
+        -(e.clientY / this.renderer.domElement.clientHeight) * 2 + 1
+      );
+      const intersects = this.raycaster.intersectObjects(this.buttons, false);
+      this.raycaster.setFromCamera(this.mouse, this.camera);
+
+      // toggles `clicked` property for only the Pickable closest to the camera
+      if (intersects.length) {
+        console.log("was clicked" + (intersects[0].object as Button).clicked);
+        (intersects[0].object as Button).clicked = !(intersects[0].object as Button).clicked;
+      }
+    });
+  }
+
+  // update loop
+  updateLoop(delta: number, clock: THREE.Clock) {
+    this.buttons.forEach((p) => {
+      p.update(delta, clock);
+    });
   }
 }
+  
--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,18 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import Stats from "three/addons/libs/stats.module.js";
-import Scene_0 from "./scene_0";
-import Scene_1 from "./scene_1";
 import Level_1 from "./level_1";
 import Level_2 from "./level_2";
 
 // //Modules to implement outline pass
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
-// import { OutlinePass } from "three/examples/jsm/postprocessing/OutlinePass.js";
-// import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
-// //Modules below are regarded to shader
-// import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
-// import { FXAAShader } from "three/examples/jsm/shaders/FXAAShader.js";
+import { OutlinePass } from "three/examples/jsm/postprocessing/OutlinePass.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+//Modules below are regarded to shader
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
+import { FXAAShader } from "three/examples/jsm/shaders/FXAAShader.js";
 
+//Project setup
 const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 100);
 camera.position.set(0.1, 2, 6);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -23,35 +22,50 @@ document.body.appendChild(renderer.domElement);
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 
-// -- shader effect
-// - composer
-const composer = new EffectComposer(renderer);
 // Scene setup
 const scenes = {
   FirstScene: "FirstScene",
   SecondScene: "SecondScene",
   None: "None",
 };
+
 let currentScene = scenes.FirstScene;
 
-//let scene_1 = new Scene_1(camera, renderer, raycaster, mouse, composer);
-//let scene_0 = new Scene_0(camera, renderer, raycaster, mouse);
-
-//scene_1.init();
-//scene_0.init();
-//await scene_1.loadAssync();
-//await scene_0.loadAssync();
-
-const level_1 = new Level_1();
-const level_2 = new Level_2();
-
+const level_1 = new Level_1(camera, renderer, raycaster, mouse);
+const level_2 = new Level_2(camera, renderer, raycaster, mouse);
+await level_1.loadAssync();
 let activeScene = level_1.scene;
+
+// -- shader effect scene 1
+let renderPass_1 = new RenderPass(level_1.scene, camera); //THE PROBLEM
+let outlinePass_1 = new OutlinePass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight), //resolution parameter
+  level_1.scene,
+  camera
+);
+
+let effectFXAA_1 = new ShaderPass(FXAAShader);
+effectFXAA_1.uniforms["resolution"].value.set(1 / window.innerWidth, 1 / window.innerHeight);
+const composer_1 = new EffectComposer(renderer);
+
+// -- shader effect scene 2
+let renderPass_2 = new RenderPass(level_2.scene, camera); //THE PROBLEM
+let outlinePass_2 = new OutlinePass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight), //resolution parameter
+  level_2.scene,
+  camera
+);
+let effectFXAA_2 = new ShaderPass(FXAAShader);
+
+effectFXAA_2.uniforms["resolution"].value.set(1 / window.innerWidth, 1 / window.innerHeight);
+
+const composer_2 = new EffectComposer(renderer);
 document.onkeydown = function (e) {
   e = e || window.event;
   if (e.shiftKey) {
+    currentScene = scenes.SecondScene;
     activeScene = level_2.scene;
   }
-  // use e.keyCode
 };
 
 document.addEventListener("StartNewScene", () => {
@@ -62,12 +76,58 @@ window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  composer.setSize(window.innerWidth, window.innerHeight);
+  composer_1.setSize(window.innerWidth, window.innerHeight);
 
-  // if (currentScene == scenes.FirstScene) {
-  //   scene_1.effectFXAA.uniforms["resolution"].value.set(1 / window.innerWidth, 1 / window.innerHeight);
-  // }
+  if (currentScene == scenes.FirstScene) {
+    effectFXAA_1.uniforms["resolution"].value.set(1 / window.innerWidth, 1 / window.innerHeight);
+  } else if (currentScene == scenes.SecondScene) {
+    effectFXAA_2.uniforms["resolution"].value.set(1 / window.innerWidth, 1 / window.innerHeight);
+  }
 });
+
+function createOutlines_1() {
+  composer_1.addPass(renderPass_1);
+  // -- parameter config
+  outlinePass_1.edgeStrength = 3.0;
+  outlinePass_1.edgeGlow = 0;
+  outlinePass_1.edgeThickness = 3.0;
+  outlinePass_1.pulsePeriod = 0;
+  outlinePass_1.usePatternTexture = false; // patter texture for an object mesh
+  outlinePass_1.visibleEdgeColor.set("#ffffff"); // set basic edge color
+  outlinePass_1.hiddenEdgeColor.set("#1abaff"); // set edge color when it hidden by other object
+  composer_1.addPass(outlinePass_1);
+
+  effectFXAA_1.renderToScreen = true;
+  composer_1.addPass(effectFXAA_1);
+}
+
+function createOutlines_2() {
+  composer_2.addPass(renderPass_2);
+  // -- parameter config
+  outlinePass_2.edgeStrength = 3.0;
+  outlinePass_2.edgeGlow = 5;
+  outlinePass_2.edgeThickness = 3.0;
+  outlinePass_2.pulsePeriod = 0;
+  outlinePass_2.usePatternTexture = false; // patter texture for an object mesh
+  outlinePass_2.visibleEdgeColor.set("#ffffff"); // set basic edge color
+  outlinePass_2.hiddenEdgeColor.set("#1abaff"); // set edge color when it hidden by other object
+  composer_2.addPass(outlinePass_2);
+
+  effectFXAA_2.renderToScreen = true;
+  composer_2.addPass(effectFXAA_2);
+}
+
+function addOutlines() {
+  outlinePass_1.selectedObjects.push(level_1.popMesh);
+  level_1.setOutlines(outlinePass_1);
+  level_2.setOutlines(outlinePass_2);
+  outlinePass_2.selectedObjects.push(level_2.textMesh);
+  outlinePass_2.selectedObjects.push(level_2.cube);
+}
+
+createOutlines_1();
+createOutlines_2();
+addOutlines();
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
@@ -85,26 +145,22 @@ const clock = new THREE.Clock();
 let delta = 0;
 
 function animate() {
-  renderer.clearColor();
   requestAnimationFrame(animate);
 
   delta = clock.getDelta();
 
-  //scene_1.updateLoop(delta, clock);
   renderer.render(activeScene, camera);
 
-  // if (currentScene == scenes.FirstScene) {
-  //   scene_1.animate(delta, clock);
-  //   renderer.render(scene_1, camera);
-  // } else if (currentScene == scenes.SecondScene) {
-  //   scene_0.animate(delta, clock);
-  //   renderer.render(scene_0, camera);
-  // }
+  if (currentScene == scenes.FirstScene) {
+    composer_1.render();
+    level_1.updateLoop(delta, clock);
+  } else if (currentScene == scenes.SecondScene) {
+    composer_2.render();
+    level_1.updateLoop(delta, clock);
+  }
   controls.update();
 
   stats.update();
-
-  composer.render();
 }
 
 
